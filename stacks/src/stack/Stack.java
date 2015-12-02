b18b70986566256dcbb4d7edf19e8ddd1da1334d
Benjami-F <b.fanjeaux@gmail.com>
2015-12-02 22:18:49 +0100

    Q1-2 - FIFO

---
+++ stacks/src/stack/Stack.java
@@ -0,0 +1,154 @@
+package stack;
+
+import java.util.Arrays;
+
+public class Stack {
+	private Integer[] stackArray = null;
+	private int size;
+	
+	/**
+	 * Constructor
+	 * @param s size of the Array
+	 */
+	public Stack(int s) throws IllegalStateException{
+		if(s<1)
+			throw new IllegalStateException("La taille de la pile doit être supérieure à 1!");
+		
+		size = s;
+		stackArray = new Integer[size];
+		for(int i=0; i<stackArray.length; i++){
+			stackArray[i] = null;
+		}
+	}
+	
+	/**
+	 * Return array size
+	 * @return size
+	 */
+	public int getSize(){
+		return stackArray.length;
+	}
+	
+	/**
+	 * Return the number of elements in the array
+	 * @return number of elements
+	 */
+	public int getNumberOfElements(){
+		int count = 0;
+		for(int i=0; i<stackArray.length; i++){
+			if(stackArray[i] != null)
+				count++;
+		}
+		
+		return count;
+	}
+	
+	/**
+	 * Check is the array is empty
+	 * @return boolean
+	 */
+	public boolean isEmpty(){
+		if(getNumberOfElements() == 0)
+			return true;
+		else 
+			return false;
+	}
+	
+	/**
+	 * Check is the array is full
+	 * @return boolean
+	 */
+	public boolean isFull(){
+		if(getNumberOfElements() == size)
+			return true;
+		else 
+			return false;
+	}
+	
+	/**
+	 * Add new int in the stack
+	 * @param x
+	 * @throws IllegalStateException
+	 */
+	public void push(int x) throws IllegalStateException{
+		
+		if(isFull())
+			throw new IllegalStateException("La pile est pleine!");
+		
+		for(int i=0; i<stackArray.length; i++){
+			if(stackArray[i] == null){
+				stackArray[i] = x;
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * Delete from stack 
+	 * @param x
+	 * @throws IllegalStateException
+	 */
+	public void pop() throws IllegalStateException{
+		
+		if(isEmpty())
+			throw new IllegalStateException("La pile est vide!");
+		
+		for(int i=stackArray.length-1; i>=0; i--){
+			if(stackArray[i] != null){
+				stackArray[i] = null;
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Return the head int of the stack
+	 * @return int
+	 * @throws IllegalStateExceptio
+	 */
+	public int head() throws IllegalStateException{
+		Integer headInt = null;
+		
+		if(isEmpty())
+			throw new IllegalStateException("La pile est vide!");
+		
+		for(int i=stackArray.length-1; i>=0; i--){
+			if(stackArray[i] != null){
+				headInt = stackArray[i];
+				break;
+			}
+		}
+		
+		return headInt;
+	}
+	
+	@Override
+	public String toString() {
+		return "Stack [stackArray=" + Arrays.toString(stackArray) + ", size=" + size + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + size;
+		result = prime * result + Arrays.hashCode(stackArray);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stack other = (Stack) obj;
+		if (size != other.size)
+			return false;
+		if (!Arrays.equals(stackArray, other.stackArray))
+			return false;
+		return true;
+	}
+}
---
+++ stacks/src/stack/StackTest.java
@@ -0,0 +1,124 @@
+package stack;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class StackTest {
+	Stack myStack;
+	
+	@Before
+	public void setUp(){
+		myStack = new Stack(5);
+	}
+	
+	@After
+	public void tearDown(){
+		myStack = null;
+	}
+	
+	/**
+	 * Test the exception throw if bad init
+	 */
+	@Test(expected=IllegalStateException.class)
+	public void testConstructorFail() {
+		myStack = new Stack(0);
+	}
+	
+	/**
+	 * Test getSize
+	 */
+	@Test
+	public void testSize() {
+		assertEquals(5, myStack.getSize());
+	}
+	
+	/**
+	 * Test getNumberOfElements
+	 */
+	@Test
+	public void testNbElem(){
+		assertEquals(0, myStack.getNumberOfElements());
+	}
+	
+	/**
+	 * Test push
+	 */
+	@Test
+	public void testPush() {
+		myStack.push(3);
+		assertEquals(1, myStack.getNumberOfElements());
+	}
+	
+	/**
+	 * Test exception throw if push overflow
+	 */
+	@Test(expected=IllegalStateException.class)
+	public void testOverflow(){
+		myStack.push(3);
+		myStack.push(4);
+		myStack.push(5);
+		myStack.push(6);
+		myStack.push(6);
+		myStack.push(6);
+	}
+	
+	/**
+	 * Test isFull
+	 */
+	@Test
+	public void testIsFull(){
+		myStack.push(3);
+		myStack.push(4);
+		myStack.push(5);
+		myStack.push(6);
+		myStack.push(6);
+		assertEquals(true, myStack.isFull());
+	}
+	
+	/**
+	 * Test exception throw empty pop
+	 */
+	@Test(expected=IllegalStateException.class)
+	public void testNullPop(){
+		myStack.pop();
+	}
+	
+	/**
+	 * Test pop
+	 */
+	@Test
+	public void testPop(){
+		myStack.push(4);
+		myStack.pop();
+		assertEquals(true, myStack.isEmpty());
+	}
+	
+	/**
+	 * Test isEmpty
+	 */
+	@Test
+	public void testIsEmpty(){
+		assertEquals(true, myStack.isEmpty());
+	}
+	
+	/**
+	 * Test exception throw if null head
+	 */
+	@Test(expected=IllegalStateException.class)
+	public void testNullHead(){
+		myStack.head();
+	}
+	
+	/**
+	 * Test head
+	 */
+	@Test
+	public void testHead(){
+		myStack.push(3);
+		assertEquals(3, myStack.head());
+	}
+}
